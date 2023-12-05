8d94d29717b35895ff5da937823e8169395dc4f6
Gamingxyz1223 <140248174+Gamingxyz1223@users.noreply.github.com>
2023-12-05 20:56:49 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -2,31 +2,37 @@ const text = document.getElementById("demo");
 const gif = document.getElementById("gif");
 
 function myFunction() {
-    text.innerHTML = "Aaaaaa, I like you too";
-    gif.src =
-        "https://raw.githubusercontent.com/DzarelDeveloper/Img/main/gif.webp";
+    text.innerHTML = "ああああああ、私もあなたが好きです";
+    gif.src = "https://media.tenor.com/OP7iKHpQyVAAAAAC/menhera-chan-love.gif";
 }
 
-function over() {
+// function tidak() {
+//     text.innerHTML = "";
+//     gif.src = "gambate.mp4";
+// }
+
+function jangan() {
     const noBtnReact = document.getElementById("noBtn").getBoundingClientRect();
     const maxX = window.innerWidth - noBtnReact.width;
     const maxY = window.innerHeight - noBtnReact.height;
-
+    // //
     const randomX = Math.floor(Math.random() * maxX);
     const randomY = Math.floor(Math.random() * maxY);
-
+    // //
     document.getElementById("noBtn").style.left = randomX + "px";
     document.getElementById("noBtn").style.top = randomY + "px";
 }
-
-function no() {
+// //
+function tidak() {
     const noBtnReact = document.getElementById("noBtn").getBoundingClientRect();
     const maxX = window.innerWidth - noBtnReact.width;
     const maxY = window.innerHeight - noBtnReact.height;
-
+    // //
     const randomX = Math.floor(Math.random() * maxX);
     const randomY = Math.floor(Math.random() * maxY);
-
+    // //
     document.getElementById("noBtn").style.left = randomX + "px";
     document.getElementById("noBtn").style.top = randomY + "px";
 }
+// //
+// 
